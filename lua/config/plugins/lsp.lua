c26b7d88d7ee1ebec5da046b5a61181a73c36b31
Aaron70 <aarongeovanny70@gmail.com>
2025-02-18 21:41:05 -0600

    Whole rewrite

---
+++ lua/config/plugins/lsp.lua
@@ -0,0 +1,81 @@
+local ensure_installed = {
+  "stylua", -- Used to format Lua code
+  "lua-language-server",
+  -- Go
+  "delve",
+  "gopls",
+  "gofumpt",
+  "golines",
+  "goimports-reviser",
+  -- Java
+  -- "jdtls",
+  -- "java-debug-adapter",
+  -- "java-test",
+  -- "cucumber_language_server",
+}
+
+function SetupFormatOnSave()
+  vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+      if not client then
+        return
+      end
+
+      ---@diagnostic disable-next-line: param-type-mismatch
+      if client.supports_method("textDocument/formatting", 0) then
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          buffer = args.buf,
+          callback = function()
+            vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+          end,
+        })
+      end
+    end,
+  })
+end
+
+return {
+  {
+    "neovim/nvim-lsp",
+    dependencies = {
+      -- Automatically install LSPs and related tools to stdpath for Neovim
+      { "williamboman/mason.nvim",                  config = true },
+      -- "williamboman/mason-lspconfig.nvim",
+      { "WhoIsSethDaniel/mason-tool-installer.nvim" },
+      -- { "j-hui/fidget.nvim", opts = {} },
+      -- "hrsh7th/cmp-nvim-lsp",
+      --
+      {
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+          library = {
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+
+        },
+      },
+      { 'saghen/blink.cmp' }
+    },
+    opts = {
+      servers = {
+        lua_ls = {},
+        gopls = {}
+      }
+    },
+    config = function(_, opts)
+      local lspconfig = require('lspconfig')
+      for server, config in pairs(opts.servers) do
+        config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+        lspconfig[server].setup(config)
+      end
+
+      require("mason").setup()
+      require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
+      SetupFormatOnSave()
+    end,
+  },
+}
---
+++ lua/config/plugins/neo-tree.lua
@@ -0,0 +1,36 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+      },
+      window = {
+        position = 'right',
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+    event_handlers = {
+      {
+        event = "file_opened",
+        handler = function(arg)
+          require "neo-tree.command".execute({
+            action = "close"
+          })
+        end,
+      }
+    }
+  },
+}
