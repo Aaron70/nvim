4ca54e1845e1bcd473b126c4b05b8bee1681a3d0
Aaron70 <aarongeovanny70@gmail.com>
2025-02-18 21:41:05 -0600

    Whole rewrite

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# List of thing TODO
+
+- Create a new README.md
+
+- Add a way to format: Json, yaml
--- lua/configs/nvimtree.lua
+++
@@ -1,9 +0,0 @@
-local options = {
-  actions = {
-    open_file = {
-      quit_on_open = true
-    }
-  }
-}
-
-return options
