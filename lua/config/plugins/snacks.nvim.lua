89153f49c991b5eac127248d175e85f8445e42b8
Aaron70 <aarongeovanny70@gmail.com>
2025-02-24 21:19:34 -0600

    Add some configuration for the snacks.notifier

--- lua/config/plugins/snacks.nvim.lua
+++ lua/config/plugins/snacks.nvim.lua
@@ -1,3 +1,50 @@
+function notifier_setup()
+  ---@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
+  local progress = vim.defaulttable()
+  vim.api.nvim_create_autocmd("LspProgress", {
+    ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+    callback = function(ev)
+      local client = vim.lsp.get_client_by_id(ev.data.client_id)
+      local value = ev.data.params
+          .value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
+      if not client or type(value) ~= "table" then
+        return
+      end
+      local p = progress[client.id]
+
+      for i = 1, #p + 1 do
+        if i == #p + 1 or p[i].token == ev.data.params.token then
+          p[i] = {
+            token = ev.data.params.token,
+            msg = ("[%3d%%] %s%s"):format(
+              value.kind == "end" and 100 or value.percentage or 100,
+              value.title or "",
+              value.message and (" **%s**"):format(value.message) or ""
+            ),
+            done = value.kind == "end",
+          }
+          break
+        end
+      end
+
+      local msg = {} ---@type string[]
+      progress[client.id] = vim.tbl_filter(function(v)
+        return table.insert(msg, v.msg) or not v.done
+      end, p)
+
+      local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+      vim.notify(table.concat(msg, "\n"), vim.log.levels.INFO, {
+        id = "lsp_progress",
+        title = client.name,
+        opts = function(notif)
+          notif.icon = #progress[client.id] == 0 and " "
+              or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+        end,
+      })
+    end,
+  })
+end
+
 return {
   {
     "folke/snacks.nvim",
@@ -14,7 +61,13 @@ return {
       indent = { enabled = true },
       input = { enabled = true },
       picker = { enabled = false },
-      notifier = { enabled = true },
+      notifier = {
+        lazy = false,
+        enabled = true,
+        config = function()
+          notifier_setup()
+        end
+      },
       quickfile = { enabled = true },
       scope = { enabled = true },
       scroll = { enabled = true },
