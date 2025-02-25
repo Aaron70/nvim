d9dd18d411e74c2fa63150ad01f0fc7359e55523
aaronv <41397746+aaron70@users.noreply.github.com>
2025-02-24 21:20:01 -0600

    Change and add keymap for show notifications history

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,21 +1,28 @@
-vim.keymap.set("i", "jk", "<ESC>l")
+local notifier = require("snacks.notifier")
+local set = vim.keymap.set
+local leader = vim.g.mapleader
+
+set("i", "jk", "<ESC>l")
 
 -- Save file
-vim.keymap.set({ "n", "v" }, "<C-s>", ":w<CR>")
-vim.keymap.set({ "i" }, "<C-s>", "<ESC>:w<CR>")
+set({ "n", "v" }, "<C-s>", ":w<CR>")
+set({ "i" }, "<C-s>", "<ESC>:w<CR>")
 
 -- Yank to clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
-vim.keymap.set({ "n", "v" }, "<leader>p", '"+p')
+set({ "n", "v" }, leader .. "y", '"+y')
+set({ "n", "v" }, leader .. "p", '"+p')
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- Notifications
+set("n", leader .. "nh", function() notifier.show_history({}) end, { desc = "Show the notification history" })
