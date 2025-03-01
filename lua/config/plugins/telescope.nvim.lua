fcfbdd963aafae29318d09f7bd942ec6d4ba8a9a
aaronv <41397746+aaron70@users.noreply.github.com>
2025-03-01 14:16:40 -0600

    Add some telescope extensions and keymaps

--- lua/config/plugins/telescope.nvim.lua
+++ lua/config/plugins/telescope.nvim.lua
@@ -4,7 +4,9 @@ return {
     tag = '0.1.8',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+      { "nvim-telescope/telescope-ui-select.nvim" },
+      { "rcarriga/nvim-notify" }
     },
     config = function()
       require('telescope').setup {
@@ -12,19 +14,37 @@ return {
           path_display = { "smart" }
         },
         extensions = {
-          fzf = {}
+          fzf = {},
+          notify = {},
+          ui_select = {
+            require("telescope.themes").get_dropdown {
+              -- even more opts
+            }
+          }
         }
       }
       require('telescope').load_extension('fzf')
+      require("telescope").load_extension("ui-select")
+      require("telescope").load_extension("notify")
+
+
+      local builtin = require("telescope.builtin")
 
       require("which-key").add({
+        -- Notifications
+        { "<leader>n", group = "[N]otifications" },
+        { "<leader>nh", rhs = function() require("telescope").extensions.notify.notify() end, mode = "n", desc = "Show [H]istory" },
+        -- { "<leader>nd", rhs = function() notifier.hide() end, mode = "n", desc = "[D]ismiss" },
+
         -- Keymaps
         { "<leader>s", group = "[S]earch" },
-        { "<leader>sg", mode = "n", rhs = require('config.telescope.multigrep').live_multigrep, desc = "[G]rep" },
-        { "<leader>s.", mode = "n", rhs = require('telescope.builtin').oldfiles, desc = "[.]Recents" },
-        { "<leader>sr", mode = "n", rhs = require('telescope.builtin').resume, desc = "[R]esume" },
-        { "<leader>sf", mode = "n", rhs = require('telescope.builtin').find_files, desc = "[F]iles" },
-        { "<leader>sh", mode = "n", rhs = require('telescope.builtin').help_tags, desc = "[H]elp Tags" },
+        { "<leader>sg", mode = "n", rhs = require('config.telescope.pickers').live_multigrep, desc = "[G]rep" },
+        { "<leader>s.", mode = "n", rhs = builtin.oldfiles, desc = "[.]Recents" },
+        { "<leader>sr", mode = "n", rhs = builtin.resume, desc = "[R]esume" },
+        { "<leader>sf", mode = "n", rhs = builtin.find_files, desc = "[F]iles" },
+        { "<leader>sh", mode = "n", rhs = builtin.help_tags, desc = "[H]elp Tags" },
+        { "<leader>sd", mode = "n", rhs = builtin.diagnostics, desc = "[D]iagnostics" },
+        { "<leader><leader>", mode = "n", rhs = builtin.buffers, desc = "[ ]Existing buffers" },
         {
           "<leader>sn",
           mode = "n",
