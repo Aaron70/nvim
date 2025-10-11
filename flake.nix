07975d23c58656001aee5da0002745d09d22b0cd
aaronv <aarongeovanny70@gmail.com>
2025-10-11 06:02:16 -0600

    test

--- flake.nix
+++ flake.nix
@@ -260,8 +260,8 @@
         # these also recieve our pkgs variable
         # see :help nixCats.flake.outputs.packageDefinitions
         settings = {
-          suffix-path = true;
-          suffix-LD = true;
+          # suffix-path = true;
+          # suffix-LD = true;
           # The name of the package, and the default launch name,
           # and the name of the .desktop file, is `nixCats`,
           # or, whatever you named the package definition in the packageDefinitions set.
@@ -272,10 +272,10 @@
           # explained below in the `regularCats` package's definition
           # OR see :help nixCats.flake.outputs.settings for all of the settings available
           wrapRc = true;
-          configDirName = "nixCats-nvim";
+          # configDirName = "nixCats-nvim";
           # neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
-          hosts.python3.enable = true;
-          hosts.node.enable = true;
+          # hosts.python3.enable = true;
+          # hosts.node.enable = true;
         };
         # enable the categories you want from categoryDefinitions
         categories = {
@@ -310,65 +310,65 @@
           };
         };
       };
-      regularCats = { pkgs, ... }@misc: {
-        settings = {
-          suffix-path = true;
-          suffix-LD = true;
-          # IMPURE PACKAGE: normal config reload
-          # include same categories as main config,
-          # will load from vim.fn.stdpath('config')
-          wrapRc = false;
-          # or tell it some other place to load
-          # unwrappedCfgPath = "/some/path/to/your/config";
-
-          # configDirName: will now look for nixCats-nvim within .config and .local and others
-          # this can be changed so that you can choose which ones share data folders for auths
-          # :h $NVIM_APPNAME
-          configDirName = "nixCats-nvim";
-
-          aliases = [ "testCat" ];
-
-          # If you wanted nightly, uncomment this, and the flake input.
-          # neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
-          # Probably add the cache stuff they recommend too.
-        };
-        categories = {
-          markdown = true;
-          general = true;
-          neonixdev = true;
-          lint = true;
-          format = true;
-          test = true;
-          go = true; # <- disabled but you could enable it with override or module on install
-          lspDebugMode = false;
-          themer = true;
-          colorscheme = "tokyonight";
-        };
-        extra = {
-          # nixCats.extra("path.to.val") will perform vim.tbl_get(nixCats.extra, "path" "to" "val")
-          # this is different from the main nixCats("path.to.cat") in that
-          # the main nixCats("path.to.cat") will report true if `path.to = true`
-          # even though path.to.cat would be an indexing error in that case.
-          # this is to mimic the concept of "subcategories" but may get in the way of just fetching values.
-          nixdExtras = {
-            nixpkgs = ''import ${pkgs.path} {}'';
-            # or inherit nixpkgs;
-          };
-          # yes even tortured inputs work.
-          theBestCat = "says meow!!";
-          theWorstCat = {
-            thing'1 = [ "MEOW" '']]' ]=][=[HISSS]]"[['' ];
-            thing2 = [
-              {
-                thing3 = [ "give" "treat" ];
-              }
-              "I LOVE KEYBOARDS"
-              (utils.mkLuaInline ''[[I am a]] .. [[ lua ]] .. type("value")'')
-            ];
-            thing4 = "couch is for scratching";
-          };
-        };
-      };
+      # regularCats = { pkgs, ... }@misc: {
+      #   settings = {
+      #     suffix-path = true;
+      #     suffix-LD = true;
+      #     # IMPURE PACKAGE: normal config reload
+      #     # include same categories as main config,
+      #     # will load from vim.fn.stdpath('config')
+      #     wrapRc = false;
+      #     # or tell it some other place to load
+      #     # unwrappedCfgPath = "/some/path/to/your/config";
+      #
+      #     # configDirName: will now look for nixCats-nvim within .config and .local and others
+      #     # this can be changed so that you can choose which ones share data folders for auths
+      #     # :h $NVIM_APPNAME
+      #     configDirName = "nixCats-nvim";
+      #
+      #     aliases = [ "testCat" ];
+      #
+      #     # If you wanted nightly, uncomment this, and the flake input.
+      #     # neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
+      #     # Probably add the cache stuff they recommend too.
+      #   };
+      #   categories = {
+      #     markdown = true;
+      #     general = true;
+      #     neonixdev = true;
+      #     lint = true;
+      #     format = true;
+      #     test = true;
+      #     go = true; # <- disabled but you could enable it with override or module on install
+      #     lspDebugMode = false;
+      #     themer = true;
+      #     colorscheme = "tokyonight";
+      #   };
+      #   extra = {
+      #     # nixCats.extra("path.to.val") will perform vim.tbl_get(nixCats.extra, "path" "to" "val")
+      #     # this is different from the main nixCats("path.to.cat") in that
+      #     # the main nixCats("path.to.cat") will report true if `path.to = true`
+      #     # even though path.to.cat would be an indexing error in that case.
+      #     # this is to mimic the concept of "subcategories" but may get in the way of just fetching values.
+      #     nixdExtras = {
+      #       nixpkgs = ''import ${pkgs.path} {}'';
+      #       # or inherit nixpkgs;
+      #     };
+      #     # yes even tortured inputs work.
+      #     theBestCat = "says meow!!";
+      #     theWorstCat = {
+      #       thing'1 = [ "MEOW" '']]' ]=][=[HISSS]]"[['' ];
+      #       thing2 = [
+      #         {
+      #           thing3 = [ "give" "treat" ];
+      #         }
+      #         "I LOVE KEYBOARDS"
+      #         (utils.mkLuaInline ''[[I am a]] .. [[ lua ]] .. type("value")'')
+      #       ];
+      #       thing4 = "couch is for scratching";
+      #     };
+      #   };
+      # };
     };
 
     defaultPackageName = "nvim";
