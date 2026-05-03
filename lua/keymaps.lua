1558597ec5479f969c37272465457a8c259d02a0
aaronv <41397746+aaron70@users.noreply.github.com>
2026-05-03 02:57:36 -0600

    Initial configuration

---
+++ lua/keymaps.lua
@@ -0,0 +1,73 @@
+-- [[ Basic Keymaps ]]
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set("i", "jk", "<Esc>", { desc = 'Go to normal mode' } )
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = 'Moves Line Down' })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'Moves Line Up' })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = 'Scroll Down' })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = 'Scroll Up' })
+vim.keymap.set("n", "n", "nzzzv", { desc = 'Next Search Result' })
+vim.keymap.set("n", "N", "Nzzzv", { desc = 'Previous Search Result' })
+
+vim.keymap.set("n", "<leader><leader>[", "<cmd>bprev<CR>", { desc = 'Previous Buffer' })
+vim.keymap.set("n", "<leader><leader>]", "<cmd>bnext<CR>", { desc = 'Next buffer' })
+vim.keymap.set("n", "<leader><leader>l", "<cmd>b#<CR>", { desc = 'Last buffer' })
+vim.keymap.set("n", "<leader><leader>d", "<cmd>bdelete<CR>", { desc = 'delete buffer' })
+
+
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left pane" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right pane" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the down pane" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the up pane" })
+
+
+-- see help sticky keys on windows
+vim.cmd([[command! W w]])
+vim.cmd([[command! Wq wq]])
+vim.cmd([[command! WQ wq]])
+vim.cmd([[command! Q q]])
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', function() vim.diagnostic.jump({ count=-1, float=true}) end, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', function() vim.diagnostic.jump({ count=1, float=true}) end, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+
+-- kickstart.nvim starts you with this. 
+-- But it constantly clobbers your system clipboard whenever you delete anything.
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+-- vim.o.clipboard = 'unnamedplus'
+
+-- You should instead use these key bindings so that they are still easy to use, but don't conflict
+vim.keymap.set({"v", "x", "n"}, '<leader>y', '"+y', { noremap = true, silent = true, desc = 'Yank to clipboard' })
+vim.keymap.set({"n", "v", "x"}, '<leader>Y', '"+yy', { noremap = true, silent = true, desc = 'Yank line to clipboard' })
+vim.keymap.set({"n", "v", "x"}, '<C-a>', 'gg0vG$', { noremap = true, silent = true, desc = 'Select all' })
+vim.keymap.set({'n', 'v', 'x'}, '<leader>p', '"+p', { noremap = true, silent = true, desc = 'Paste from clipboard' })
+vim.keymap.set('i', '<C-p>', '<C-r><C-p>+', { noremap = true, silent = true, desc = 'Paste from clipboard from within insert mode' })
+vim.keymap.set("x", "<leader>P", '"_dP', { noremap = true, silent = true, desc = 'Paste over selection without erasing unnamed register' })
+
+-- Lua Development
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>", { desc = "Source the current buffer" })
+vim.keymap.set("n", "<space>x", ":.lua<CR>", { desc = "Executes the current lua buffer" })
+vim.keymap.set("v", "<space>x", ":lua<CR>",  { desc = "Executes the selected lua code" })
+
+
+-- Toggle Harper LSP
+vim.keymap.set('n', '<leader>H', function()
+  local name = "harper_ls"
+  vim.lsp.enable(name, not vim.lsp.is_enabled(name))
+  if vim.lsp.is_enabled(name) then
+    vim.notify("Harper LSP enabled")
+  else
+    vim.notify("Harper LSP disabled")
+  end
+end, { desc = 'Toogle [H]arper LSP' })
---
+++ plugin/blink.cmp.lua
@@ -0,0 +1,116 @@
+vim.pack.add({
+  { src = "https://github.com/folke/lazydev.nvim" },
+  { src = "https://github.com/L3MON4D3/LuaSnip" },
+  { src = "https://github.com/xzbdmw/colorful-menu.nvim" },
+  { src = "https://github.com/saghen/blink.compat" },
+  { src = "https://github.com/hrsh7th/cmp-cmdline" },
+  { src = "https://github.com/saghen/blink.cmp", version = "v1", name = "blink.cmp" },
+})
+
+require("luasnip").setup({})
+require('luasnip.loaders.from_vscode').lazy_load()
+
+local cmp = require('blink.cmp')
+-- cmp.build():wait(60000)
+cmp.setup({
+  -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+  -- See :h blink-cmp-config-keymap for configuring keymaps
+  keymap = {
+    preset = 'default',
+    ['<CR>'] = { 'accept', 'fallback' },
+  },
+  cmdline = {
+    enabled = true,
+    completion = {
+      menu = {
+        auto_show = true,
+      },
+    },
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+    },
+    -- sources = function()
+    --   local type = vim.fn.getcmdtype()
+    --   -- Search forward and backward
+    --   if type == '/' or type == '?' then return { 'buffer' } end
+    --   -- Commands
+    --   if type == ':' or type == '@' then return { 'cmdline', 'cmp_cmdline' } end
+    --   return {}
+    -- end,
+  },
+  fuzzy = {
+    sorts = {
+      'exact',
+      -- defaults
+      'score',
+      'sort_text',
+    },
+  },
+  signature = {
+    enabled = true,
+    window = {
+      show_documentation = true,
+    },
+  },
+  completion = {
+    menu = {
+      draw = {
+        treesitter = { 'lsp' },
+        components = {
+          label = {
+            text = function(ctx)
+              return require("colorful-menu").blink_components_text(ctx)
+            end,
+            highlight = function(ctx)
+              return require("colorful-menu").blink_components_highlight(ctx)
+            end,
+          },
+        },
+      },
+    },
+    documentation = {
+      auto_show = true,
+    },
+  },
+  snippets = {
+    preset = 'luasnip',
+    active = function(_)
+      local snippet = require "luasnip"
+      local blink = require "blink.cmp"
+      if snippet.in_snippet() and not blink.is_visible() then
+        return true
+      else
+        if not snippet.in_snippet() and vim.fn.mode() == "n" then snippet.unlink_current() end
+        return false
+      end
+    end,
+  },
+  sources = {
+    default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer', 'omni' },
+    providers = {
+      path = {
+        score_offset = 50,
+      },
+      lsp = {
+        score_offset = 40,
+      },
+      snippets = {
+        score_offset = 40,
+      },
+      cmp_cmdline = {
+        name = 'cmp_cmdline',
+        module = 'blink.compat.source',
+        score_offset = -100,
+        opts = {
+          cmp_name = 'cmdline',
+        },
+      },
+      lazydev = {
+        name = "LazyDev",
+        module = "lazydev.integrations.blink",
+        -- make lazydev completions top priority (see `:h blink.cmp`)
+        score_offset = 100,
+      },
+    },
+  },
+})
