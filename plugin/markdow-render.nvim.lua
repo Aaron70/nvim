0e90560a96bebe571bc59a0b21c6def56c9e2b61
aaronv <41397746+aaron70@users.noreply.github.com>
2026-06-07 10:18:24 -0600

    Disable indenting on markdown renderer

--- plugin/markdow-render.nvim.lua
+++ plugin/markdow-render.nvim.lua
@@ -7,6 +7,6 @@ require("render-markdown").setup({
   render_modes = true,
   code = { language_border = ' ', language_left = '', language_right = '', sign = false },
   pipe_table = { preset = 'round' },
-  indent = { enabled = true, skip_heading = false, icon = ' ', sign = false },
+  indent = { enabled = false, skip_heading = false, icon = ' ', sign = false },
   heading = { position = 'inline', left_pad = 1, sign = false },
 })
