a9f8cde94e115efd53670b0b6111449c9e75bd37
aaronv <41397746+aaron70@users.noreply.github.com>
2026-07-11 16:35:27 -0600

    Update pathfinder.nvim to fix go doc links

--- plugin/pathfinder.lua
+++ plugin/pathfinder.lua
@@ -1,6 +1,8 @@
 vim.pack.add({
-  { src = "https://github.com/HawkinsT/pathfinder.nvim" },
+  {
+    src = "https://github.com/Aaron70/pathfinder.nvim", -- FIX: Change this to the original repository once the issue is solved 
+    version = "feat/support-go-doc-link"
+  },
 })
-
 
 require("pathfinder").setup({})
